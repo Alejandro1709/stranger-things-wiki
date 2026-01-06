ac7ad3afaa1e55992926b9e62f4b1aef7c1954b2
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 16:10:21 -0500

    Add: ability to fetch characters from api

--- src/actions/characters.action.ts
+++ src/actions/characters.action.ts
@@ -1,7 +1,8 @@
 import { charactersApi } from '@/api/characters.api'
+import type { ICharacter } from '@/types/character'
 
-export const getCharacters = async () => {
-  const { data } = await charactersApi.get('/')
+export const getCharacters = async (): Promise<ICharacter[]> => {
+  const { data } = await charactersApi.get<ICharacter[]>('/')
 
   return data
 }
