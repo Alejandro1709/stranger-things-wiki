1b7900f8efca21e9c386d3d3a38fba69ff2197fd
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 15:57:43 -0500

    Add: react query and devtools

--- src/WikiApp.tsx
+++ src/WikiApp.tsx
@@ -1,11 +1,16 @@
 import { RouterProvider } from 'react-router'
 import { appRouter } from './routers/app.router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const client = new QueryClient()
 
 function WikiApp() {
   return (
-    <>
+    <QueryClientProvider client={client}>
       <RouterProvider router={appRouter} />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
