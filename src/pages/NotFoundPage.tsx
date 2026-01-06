0838f59aacb7e6911a31574a9d9ff28c11f9cb6d
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 14:13:33 -0500

    Add: Notfound page layout

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,16 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router'
+
 function NotFoundPage() {
-  return <div>NotFoundPage</div>
+  return (
+    <div className="space-y-6">
+      <h1 className="text-3xl font-primary">Esta página no existe</h1>
+
+      <Button variant="default" asChild>
+        <Link to="/">Ir al inicio</Link>
+      </Button>
+    </div>
+  )
 }
 
 export default NotFoundPage
